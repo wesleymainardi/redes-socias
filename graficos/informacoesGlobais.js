5760976e63ab3f05c74349ea20f0b044bceb3fc9
wesleymainardi <129780950+wesleymainardi@users.noreply.github.com>
2024-10-16 10:01:06 -0300

    .

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,13 @@
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    cosole.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio} horas conectadas.`
+    console.log(paragrafo)
+    const container = document.getElementById(‘graficos-container’)
+    container.appendChild(paragrafo)
+  }
+  
+  vizualizarInformacoesGlobais()
